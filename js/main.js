d909e3ecfe0a23e47a0651c5fd0dba9596439e53
fermin romero <ferminromero@hotmail.com>
2022-08-16 19:11:32 -0300

    Optimizando el proyecto final

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-// Segunda entrega Proyecto Final
+// Optimizando el proyecto final
+
 let carritoIcono = document.querySelector("#carrito")
 let carrito = document.querySelector(".mi-carrito")
 let cerrarCarrito = document.querySelector(".cerrar-carrito")
@@ -6,9 +7,11 @@ let precioTotal = document.getElementById("precioTotal")
 let contadorCarrito = document.getElementById("contadorCarrito")
 let botonComprar = document.getElementById("btnComprar")
 
-
-const compraProcesada = ()=>{
+/* Cuando tocamos el boton de "Comprar", se resetea el carrito */
+const compraProcesada = () => {
     alert("¡Su orden fue procesada!")
+    /* Spread del Carrito */
+    console.log(...carro)
     carro.length = 0
     actualizarCarrito()
 }
@@ -39,11 +42,10 @@ productos.forEach((producto) => {
     const div = document.createElement("div")
     div.classList.add("caja-carrito")
     div.innerHTML = `
-    <div class="producto" id="producto">
-    <img src="${producto.img}"  alt="imagen producto" class="imagen-producto">
-    <h2 class="titulo">${producto.nombre}</h2>
-    <p class="precio">$${producto.precio}</p>
-    <button class="agregar-carrito" id="agregar${producto.id}">Agregar al Carrito</button>
+    <img src="${img}"  alt="imagen producto" class="imagen-producto">
+    <h2 class="titulo">${nombre}</h2>
+    <p class="precio">$${precio}</p>
+    <button class="agregar-carrito" id="agregar${id}">Agregar al Carrito</button>
 </div>`
     contenedorProductos.appendChild(div)
 
@@ -54,29 +56,34 @@ productos.forEach((producto) => {
     })
 })
 
+// Agrego producto al carrito depende de su ID
+
 const agregarAlCarrito = (prodId) => {
     const existe = carro.some(prod => prod.id === prodId)
     if (existe) {
         const prod = carro.map(prod => {
-            if (prod.id === prodId) {
-                prod.cantidad++
-            }
-        })
-    }
+             /* Operador Lógico AND */
+             prod.id === prodId && prod.cantidad++
+            })
+        }
+    
     else {
         const item = productos.find((prod) => prod.id === prodId)
         carro.push(item)
         console.log(carro)
     }
-}
 
+// elimina el producto
 const eliminarDelCarrito = (prodId) => {
     const item = carro.find((prod) => prod.id === prodId)
     const index = carro.indexOf(item)
+    carro[index].cantidad = 0
+    //Elimino el producto del array
     carro.splice(index, 1)
     actualizarCarrito()
 }
 
+// Contenedor donde van a ir los items
 const contenedorCarrito = document.getElementById("carritoContenido")
 
 const actualizarCarrito = () => {
@@ -84,19 +91,24 @@ const actualizarCarrito = () => {
     carro.forEach((prod) => {
         const div = document.createElement("div")
         div.className = ("caja-carrito")
+        let { img, nombre, precio, cantidad, id } = prod
         div.innerHTML = `
-        <img src="${prod.img}" alt="producto1" class="carrito-imagen">
+        <img src="${img}" alt="producto1" class="carrito-imagen">
         <div class="detalles">
-            <div class="titulo-producto">${prod.nombre}</div>
-            <div id="precioCarrito" class="precio-carrito">${prod.precio}</div>
-            <input readonly = "readonly" id="cantidadCarrito" type="number" value="${prod.cantidad}" class="cantidad-carrito">
-        </div>
-        <img class="remover-carrito" onclick = "eliminarDelCarrito(${prod.id})"
+        <div class="titulo-producto">${nombre}</div>
+        <div id="precioCarrito" class="precio-carrito">${precio}</div>
+        <input readonly = "readonly" id="cantidadCarrito" type="number" value="${cantidad}" class="cantidad-carrito">
+    </div>
+    <img class="remover-carrito" onclick = "eliminarDelCarrito(${id})"
             src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAJ9JREFUSEvt1csRwjAMRdGTSigB6IQSSAeUQCdQAp0AHZBKYFjwCzFyMgmbREuP5l3p2ZYKA0cxsL4cwBq7RCEl9r+KjAALHIMulzilcuqAa0+WPXX/DuipgZdM6g66WvWlNw7Ao8u6bU3nnSyaAI1P/93vyaIRWNRmAGZ/tAtmbZRxxn1BfURq2K2wxTwTUmGDQy4gUzdOi3ZyrBBk3AB+wyoZJl1mqQAAAABJRU5ErkJggg==" />
         `
         contenedorCarrito.appendChild(div)
+        //Guardo el prodcuto en local Stroage
         localStorage.setItem("carro", JSON.stringify(carro))
     })
+    
     contadorCarrito.innerText = carro.length
-    precioTotal.innerText = carro.reduce((acc, prod) => acc + prod.precio, 0)
+    // Junto todos los precios
+    precioTotal.innerText = carro.reduce((acc, prod) => acc + prod.precio * prod.cantidad, 0)
+}
 }
